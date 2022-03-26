21c80259d54ce35b956c0723bd31825579a1501f
Tilok Paul <tilokpaul.web@gmail.com>
2022-03-26 23:09:10 +0600

    Update about react  question answer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const reactAbout = [
   {
     id: "q1",
     heading: 'How react works?',
-    paragraph: 'React is JavaScript library for creating user interface by rendering the virtual DOM and React Diffing algorithm . All component in React has a life cycle which manipulate 3 main phases are (Mounting, Updating, Unmounting). Mounting step elements are putting into the DOM. Rendering template by comparing Virtual DOM with Real DOM and display rendered component (Data, html template). '
+    paragraph: 'React is JavaScript library for creating user interface by rendering the virtual DOM and React Diffing algorithm. The virtual DOM is a represent of Real DOM object. Virtual DOM more lightweight from Browser DOM. Stage change compute the virtual DOM JS Tree with Browser DOM JS tree and rendered the recollection of the updated js tree. All component in React has a life cycle which manipulate 3 main phases are (Mounting, Updating, Unmounting). Mounting step elements are putting into the DOM. Rendering template by comparing Virtual DOM with Real DOM and display rendered component (Data, html template). '
   },
   {
     id: "q2",
@@ -16,8 +16,8 @@ const reactAbout = [
   },
   {
     id: "q3",
-    heading: 'How use state works?',
-    paragraph: 'UseState is a function which know as "Hook" that allow to keep state variable where need a function component. It allows to declare read and update this state value. If any changes in the current state variable re-render the the component and display the update changes. '
+    heading: 'How useState works?',
+    paragraph: 'useState is a function which know as "Hook" that allow to keep state variable where need a function component.Three part of useState hooks 1: Name of the state 2: Function if change value of this sate which will update 3: The initial value of the state. Variable can be used to get the state value and setState can be used to update the state. It allows to declare read and update this state value. If any changes in the current state variable re-render the the component and display the update changes. When we call setStateName() and try to update the state value.'
   }
 
 ]
